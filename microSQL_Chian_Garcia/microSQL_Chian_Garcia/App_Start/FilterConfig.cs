3103b0ad8bdfa395361ebd0cf01b006133f477d5
PJGM14 <pablitolink@hotmail.com>
2019-04-11 07:34:53 -0600

    Proyecto vacio

---
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace microSQL_Chian_Garcia
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="microSQL_Chian_Garcia.MvcApplication" Language="C#" %>
