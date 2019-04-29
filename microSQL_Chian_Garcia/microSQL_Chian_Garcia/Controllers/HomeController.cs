70cfcbd2f2aaaa76790e3adefd2de53f70a1c44d
Ricardo Chian <ricardochianc@gmail.com>
2019-04-28 20:22:27 -0600

    Eliminacion de elementos predeterminados

--- microSQL_Chian_Garcia/microSQL_Chian_Garcia/Controllers/HomeController.cs
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace microSQL_Chian_Garcia.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
