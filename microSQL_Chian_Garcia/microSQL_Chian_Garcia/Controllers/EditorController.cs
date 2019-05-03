de00be7688dc0e2576d6c6da3b78b7478ab5d529
Ricardo Chian <ricardochianc@gmail.com>
2019-05-03 15:45:21 -0600

    Data agregada, Cambios en vistas y eliminacion de css inecesarios

--- microSQL_Chian_Garcia/microSQL_Chian_Garcia/Controllers/EditorController.cs
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/Controllers/EditorController.cs
@@ -14,70 +14,14 @@ namespace microSQL_Chian_Garcia.Controllers
             return View();
         }
 
-        // GET: Editor/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Editor/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         // POST: Editor/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Ejecutar(FormCollection collection)
         {
             try
             {
                 var texto = collection["TextoPantalla"];
                 // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Editor/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Editor/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Editor/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Editor/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
             }
---
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/Instancia/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using microSQL_Chian_Garcia.Models;
+
+namespace microSQL_Chian_Garcia.Instancia
+{
+    public class Data
+    {
+        private static Data _instancia = null;
+
+        public static Data Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new Data();
+                }
+
+                return _instancia;
+            }
+        }
+
+        public Editor EditorTexto = new Editor("");
+    }
+}
