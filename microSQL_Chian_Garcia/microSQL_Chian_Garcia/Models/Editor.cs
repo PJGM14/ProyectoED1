85601e3f4f29657931db8974d7b8b54c3828d762
PJGM14 <pablitolink@hotmail.com>
2019-05-09 03:45:17 -0600

    VALIDACIONES DE LA EDICION

--- microSQL_Chian_Garcia/microSQL_Chian_Garcia/Models/Editor.cs
+++ microSQL_Chian_Garcia/microSQL_Chian_Garcia/Models/Editor.cs
@@ -30,6 +30,7 @@ namespace microSQL_Chian_Garcia.Models
 
         public bool EjecucionCorrecta { get; set; }
 
+        //CONSTRUCTOR
         public Editor()
         {
             PalabrasReservadas = new Dictionary<string, string>();
@@ -48,6 +49,364 @@ namespace microSQL_Chian_Garcia.Models
             EjecucionCorrecta = false;
         }
 
+        //*********************************************************************************************************************************************
+
+        public void Read(string code)
+        {
+            Dictionary<string, string> crearTablaValores = new Dictionary<string, string>();
+            List<string> insertarEnTablaValores = new List<string>();
+            List<string> seleccionarDeTabla = new List<string>();
+            List<string> borrarDeTabla = new List<string>();
+            string borrarTabla;
+            
+            code = code.ToUpper();
+            code = code.Trim();
+
+            //SE REEMPLAZA TODO EL CODIGO PARA QUE QUEDE EN UNA SOLA LINEA
+            code = code.Replace(",", ", ");
+            code = code.Replace("\n", " ");
+            code = code.Replace("(", " ( ");
+            code = code.Replace(")", " ) ");
+            code = code.Replace("\r", "");
+            code = code.Replace("  ", " ");
+            code = code.Replace("VARCHAR ( 100 ) ", "VARCHAR(100)");
+
+            try
+            {
+                //SI CONTIENE UN GO O EL EQUIVALENTE EN OTRO IDIOMA
+                if (code.Contains("GO") || code.Contains(PalabrasReservadas["GO"]))
+                {
+                    //SEPARA EL CODIGO POR INSTRUCCIONES
+                    var codeInstructions = code.Split(new string[] {"GO"}, StringSplitOptions.None);
+
+                    if (codeInstructions[codeInstructions.Length - 1].Trim().Equals(""))
+                    {
+                        for(int i = 0; i < codeInstructions.Length - 1; i++)
+                        {
+                            //SEPARA CADA INSRUCCION EN PALABRAS
+                            codeInstructions[i] = codeInstructions[i].Trim();
+                            var instructionWords = codeInstructions[i].Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                            int longitud = instructionWords.Length;
+
+                            //***********************************SE VALIDA A QUE INSTRUCCION PERTENECE************************************
+                            //CREATE TABLE
+                            if ((instructionWords[0] + instructionWords[1]).Equals("CREATETABLE") || (instructionWords[0] + instructionWords[1]).Equals(PalabrasReservadas["CREATETABLE"]))
+                            {
+                                CreateTable(ref crearTablaValores, ref instructionWords, longitud);
+                            }
+
+                            //INSERT INTO
+                            if((instructionWords[0] + instructionWords[1]).Equals("INSERTINTO") || (instructionWords[0] + instructionWords[1]).Equals(PalabrasReservadas["INSERTINTO"]))
+                            {
+                                InsertInto(ref insertarEnTablaValores, ref instructionWords, longitud);
+                            }
+
+                            //SELECT FROM
+                            if(instructionWords[0].Equals("SELECT") || instructionWords[0].Equals(PalabrasReservadas["SELECT"]))
+                            {
+                                SelectFrom(ref seleccionarDeTabla, ref instructionWords, longitud);
+                            }
+
+                            //DELETE FROM
+                            if(instructionWords[0].Equals("DELETE") || instructionWords[0].Equals(PalabrasReservadas["DELETE"]))
+                            {
+                                if(instructionWords[1].Equals("FROM") || instructionWords[1].Equals(PalabrasReservadas["FROM"]))
+                                {
+                                    DeleteFrom(ref borrarDeTabla, ref instructionWords, longitud);
+                                }
+                                
+                            }
+
+                            //DROP TABLE
+                            if((instructionWords[0] + instructionWords[1]).Equals("DROPTABLE") || (instructionWords[0] + instructionWords[1]).Equals(PalabrasReservadas["DROPTABLE"]))
+                            {
+                                borrarTabla = instructionWords[2];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //LE FALTA EL ULTIMO GO
+                    }
+                             
+
+                }
+                else
+                {
+                    //ERROR PORQUE NO HAY GO's
+
+                }
+            }
+            catch
+            {
+
+            }
+
+        
+
+        }
+
+        //******************METODO PARA ANALIZAR LAS INSTRUCCIONES PARA CREAR TABLAS*******************************************
+        public void CreateTable(ref Dictionary<string,string> crearTablaValores, ref string[] instructionWords, int longitud)
+        {
+            if (instructionWords[longitud - 1].Equals(")"))
+            {                            
+                int cantVar = 0;
+                int cantInt = 0;
+                int cantDate = 0;
+
+                crearTablaValores.Add(instructionWords[2].ToString(), "NOMBRE");
+
+                if (instructionWords[3].Equals("("))
+                {
+                    if (instructionWords[4].Equals("ID"))
+                    {
+                        if((instructionWords[5] + instructionWords[6] + instructionWords[7]).Equals("INTPRIMARYKEY,"))
+                        {
+                            crearTablaValores.Add("ID", "INT PRIMARY KEY");
+
+                            var posicion = 8;
+                            while(instructionWords[posicion] != ")")
+                            {
+                                if(posicion == (longitud - 3))
+                                {
+                                    if (instructionWords[posicion + 1].Equals("INT"))
+                                    {
+                                        if(cantInt <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "INT");
+                                            cantInt++;
+                                        }                                                    
+                                    }
+                                    if (instructionWords[posicion + 1].Equals("VARCHAR(100)"))
+                                    {
+                                        if(cantVar <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "VARCHAR(100)");
+                                            cantVar++;
+                                        }
+                                    }
+                                    if (instructionWords[posicion + 1].Equals("DATETIME"))
+                                    {
+                                        if(cantDate <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "DATETIME");
+                                            cantDate++;
+                                        }                                                    
+                                    }
+                                    posicion = longitud-1;
+                                }
+                                else
+                                {
+                                    if (instructionWords[posicion + 1].Equals("INT,"))
+                                    {
+                                        if(cantInt <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "INT");
+                                            cantInt++;
+                                        } 
+                                    }
+                                    if (instructionWords[posicion + 1].Equals("VARCHAR(100),"))
+                                    {
+                                        if(cantVar <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "VARCHAR(100)");
+                                            cantVar++;
+                                        }
+                                    }
+                                    if (instructionWords[posicion + 1].Equals("DATETIME,"))
+                                    {
+                                        if(cantDate <= 3)
+                                        {
+                                            crearTablaValores.Add(instructionWords[posicion], "DATETIME");
+                                            cantDate++;
+                                        } 
+                                    }
+                                    posicion += 2;
+                                }
+                            }                                    
+                        }
+                    }
+                    else
+                    {
+                        //ESTA MAL ESCRITO
+                    }
+                }
+                else
+                {
+                    //ESTA MAL ESCRITO LE FALTA EL PARENTESIS DE APERTURA
+                }
+            }
+            else
+            {
+                //ESTA MAL ESCRITO LE FALTA EL PARENTESIS DE CIERRE
+            }
+        }
+
+        //*****************METODO PARA ANALIZAR LAS INSTRUCCIONES PARA INSERTAR EN TABLAS**************************************
+        public void InsertInto(ref List<string> insertarEnTablaValores, ref string[] instructionWords, int longitud)
+        {
+            if (instructionWords[longitud - 1].Equals(")"))
+            {
+                insertarEnTablaValores.Add(instructionWords[2].ToString());
+
+                if (instructionWords[3].Equals("("))
+                {
+                    if (instructionWords[4].Equals("ID,"))
+                    {
+                        insertarEnTablaValores.Add("ID");
+
+                        int posicion = 5;
+
+                        while(instructionWords[posicion] != ")")
+                        {
+                            string[] sinComa = instructionWords[posicion].Split(',');
+                            insertarEnTablaValores.Add(sinComa[0]);
+                            posicion++;
+                        }
+
+                        posicion++;
+
+                        if (instructionWords[posicion].Equals("VALUES") || instructionWords[posicion].Equals(PalabrasReservadas["VALUES"]))
+                        {
+                            posicion++;
+
+                            if (instructionWords[posicion].Equals("("))
+                            {
+                                posicion++;
+
+                                while(instructionWords[posicion] != ")")
+                                {
+                                    string[] sinComa = instructionWords[posicion].Split(',');
+
+                                    if (sinComa[0].Contains("'"))
+                                    {
+                                        string[] sinComillas = sinComa[0].Split('\'');
+
+                                        if(sinComillas.Length == 3)
+                                        {
+                                            insertarEnTablaValores.Add(sinComillas[1]);
+                                        }
+                                        else
+                                        {
+                                            //FALTA UNA COMILLA
+                                        }
+                                    }
+                                    else
+                                    {
+                                        insertarEnTablaValores.Add(sinComa[0]);
+                                    }
+
+                                    posicion++;
+                                }
+                            }
+                            else
+                            {
+                                //FALTA PARENTESIS DE APERTURA
+                            }
+                        }
+                        else
+                        {
+                            //FALTA LA PALABRA RESERVADA PARA INSERTAR VALORES
+                        }
+                    }
+                    else
+                    {
+                        //FALTAN CAMPOS PARA INSERTAR LA INFORMACION
+                    }
+                }
+                else
+                {
+                    //FALTA PARENTESIS DE APERTURA
+                }
+            }
+            else
+            {
+                //FALTA PARENTESIS DE CIERRE
+            }
+        }
+
+        //*****************METODO PARA ANALIZAR LAS INSTRUCCIONES PARA SELECCIONAR INFORMACION*********************************
+        public void SelectFrom(ref List<string> seleccionarDeTabla, ref string[] instructionWords, int longitud)
+        {
+            int posicion = 1;
+            bool contieneFrom = false;
+            bool contieneWhere = false;
+
+            for(int i = 0; i < longitud; i++)
+            {
+                if(instructionWords[i].Contains("FROM") || instructionWords[i].Contains(PalabrasReservadas["FROM"])){
+                    contieneFrom = true;
+                }
+            }
+            if (contieneFrom)
+            {
+                while(instructionWords[posicion] != "FROM" || instructionWords[posicion] != PalabrasReservadas["FROM"])
+                {
+                    if (instructionWords[posicion].Contains(","))
+                    {
+                        string[] palabraSinComa = instructionWords[posicion].Split(',');
+                        seleccionarDeTabla.Add(palabraSinComa[0]);
+                        posicion++;
+                    }
+                    else
+                    {
+                        seleccionarDeTabla.Add(instructionWords[posicion]);
+                        posicion++;
+                    }                
+                }
+                seleccionarDeTabla.Add(instructionWords[++posicion]);
+
+                if(posicion < longitud)
+                {
+                    for(int i = ++posicion; i < longitud; i++)
+                    {
+                        if(instructionWords[i].Contains("WHERE") || instructionWords[i].Contains(PalabrasReservadas["WHERE"]))
+                        {
+                            contieneWhere = true;
+                        }
+                    }
+                    if (contieneWhere)
+                    {
+                        seleccionarDeTabla.Add(instructionWords[longitud-1]);
+                    }
+                }
+            }
+            else
+            {
+                //NO CONTIENE LA PALABRA FROM, POR LO TANTO NO ESTA BIEN ESCRITO
+            }
+            
+        }
+
+        //*****************METODO PARA ANALIZAR LAS INSTRUCCIONES PARA BORRAR INFORMACION**************************************
+        public void DeleteFrom(ref List<string> borrarDeTabla, ref string[] instructionWords, int longitud)
+        {
+            bool contieneWhere = false;
+            borrarDeTabla.Add(instructionWords[2]);
+
+            for(int i = 0; i < instructionWords.Length; i++)
+            {
+                if(instructionWords[i].Equals("WHERE") || instructionWords[i].Equals(PalabrasReservadas["WHERE"]))
+                {
+                    contieneWhere = true;
+                }
+            }
+            if (contieneWhere)
+            {
+                borrarDeTabla.Add(instructionWords[longitud-1]);
+            }
+        }
+
+        //
+        public void DropTable()
+        {
+
+        }
+        
+        /// <summary>
+        /// .******************************************************************************************************************************************
+        /// </summary>
         //Método que obtiene la información de cada tabla que está en la carpeta de ~MicroSQL/Tablas
         public void ObtenerTablas()
         {
